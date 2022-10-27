36e044fd991f47e59914def5bac1b17a69307cb0
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-27 12:23:25 +0600

    Footer UI

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "./../assets/img/blog-logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer>
+        <img src={Logo} alt="footer-logo-img" />
+        <p>
+          Made with ❤ by{" "}
+          <b>
+            <i>Mohitul Islam.</i>
+          </b>{" "}
+          &nbsp;
+          <Link
+            className="linkUrl"
+            to="https://github.com/mohit838/Dyanmic-Blog"
+          >
+            | GitHub &nbsp;
+          </Link>
+          <Link className="linkUrl" to="https://facebook.com/mohit838">
+            | Fabcebook |
+          </Link>
+        </p>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
