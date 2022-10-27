de82198f76dc99eddb41a0a5b67091042111a0de
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-27 14:10:33 +0600

    Fake Data Upload

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import BlogApi from "../assets/data/BlogApi";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="home">
+        <div className="posts">
+          {BlogApi.map((value, index) => {
+            return (
+              <div className="post" key={index}>
+                <div className="img">
+                  <img src={value.img} alt="post-img" />
+                </div>
+                <div className="content">
+                  <Link to={`/post/${index}`}>
+                    <h1>{value.title}</h1>
+                  </Link>
+                  <p>{value.desc}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
