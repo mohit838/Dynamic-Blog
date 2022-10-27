2bf6389752153825e6ed833236607260313a08d3
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-27 11:26:09 +0600

    Register page UI

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <div className="login">
+        <h1>Register</h1>
+        <form>
+          <input required type="text" placeholder="Username*" />
+          <input required type="email" placeholder="Email*" />
+          <input required type="password" placeholder="Password*" />
+          <button>Register</button>
+          <p>This is an error!</p>
+          <span>
+            Already have any account? <Link to="/login">LogIn</Link>
+          </span>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Register;
