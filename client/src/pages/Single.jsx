92b55e8656299e51aa92741dfa99e9e8bf279821
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-27 16:29:32 +0600

    Single page edit delete

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaRegEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <>
+      <div className="single">
+        <div className="content">
+          <img
+            src="https://i.ibb.co/Rz5pMkG/image-webp-1.webp"
+            alt="single-img"
+          />
+          <div className="user">
+            <img src="https://i.ibb.co/Jxcc5Km/mohit2.jpg" alt="user-img" />
+
+            <div className="info">
+              <span>Mohit</span>
+              <p>Posted 1 day ago</p>
+            </div>
+
+            <div className="edit">
+              <Link to={`/write?edit=2`}>
+                <FaRegEdit />
+              </Link>
+              <Link to={`/write?delete=2`}>
+                <MdDelete />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="menu">m</div>
+      </div>
+    </>
+  );
 };
 
 export default Single;
