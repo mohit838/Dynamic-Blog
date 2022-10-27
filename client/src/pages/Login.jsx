6f8e6e04556576f715031dd881d8def20bc99ff3
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-27 11:21:18 +0600

    Update Login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="login">
-      <h1>LogIn</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button>LogIn</button>
-        <p>Username or password is worng!</p>
-        <span>
-          Don't have any account? <Link to="/register">Register</Link>
-        </span>
-      </form>
-    </div>
+    <>
+      <div className="login">
+        <h1>LogIn</h1>
+        <form>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button>LogIn</button>
+          <p>Username or password is worng!</p>
+          <span>
+            Don't have any account? <Link to="/register">Register</Link>
+          </span>
+        </form>
+      </div>
+    </>
   );
 };
 
