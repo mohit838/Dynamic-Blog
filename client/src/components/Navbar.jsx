832dfe0ba3f30d31b5cc4a4f0073aab0361d57a1
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-27 12:05:41 +0600

    Navbar UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "./../assets/img/blog-logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div className="navbar">
+        <div className="container">
+          <div className="logo">
+            <img src={Logo} alt="logo-img" />
+          </div>
+          <div className="links">
+            <Link className="linkUrl" to="/?cat=art">
+              <h6>Art</h6>
+            </Link>
+            <Link className="linkUrl" to="/?cat=craft">
+              <h6>Craft</h6>
+            </Link>
+            <Link className="linkUrl" to="/?cat=technology">
+              <h6>Technology</h6>
+            </Link>
+            <Link className="linkUrl" to="/login">
+              <h6>LogIn</h6>
+            </Link>
+            <Link className="linkUrl" to="/register">
+              <h6>Register</h6>
+            </Link>
+            <span>Mohit</span>
+            <span>LogOut</span>
+            <div className="write">
+              <Link className="linkUrl" to="/write">
+                Write
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
