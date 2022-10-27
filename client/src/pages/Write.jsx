7c83252cc68a62b7ec9233816f873fcabbb4c90a
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-27 18:49:11 +0600

    Working with blog writing page

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,7 +1,61 @@
-import React from "react";
-
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 const Write = () => {
-  return <div>Write</div>;
+  const [value, setValue] = useState("");
+
+  console.log(value);
+
+  return (
+    <>
+      <div className="write">
+        <div className="content">
+          <input type="text" placeholder="Title" />
+
+          <div className="editorContainer">
+            <ReactQuill
+              className="editorBlog"
+              theme="snow"
+              value={value}
+              onChange={setValue}
+            />
+          </div>
+        </div>
+        <div className="menu">
+          <div className="item">
+            <h1>Publish</h1>
+            <span>
+              <b>Status: </b> Draft
+            </span>
+            <span>
+              <b>Visibility: </b> Public
+            </span>
+            <input style={{ display: "none" }} type="file" name="" id="file" />
+            <label htmlFor="file">Upload</label>
+            <div className="buttons">
+              <button>Save as draft</button>
+              <button>Udpate</button>
+            </div>
+          </div>
+          <div className="item">
+            <div className="categories">
+              <input type="radio" name="cat" value="art" id="art" />
+              <label htmlFor="art">ART</label>
+              <input
+                type="radio"
+                name="cat"
+                value="technology"
+                id="technology"
+              />
+              <label htmlFor="technology">TECHNOLOGY</label>
+              <input type="radio" name="cat" value="craft" id="craft" />
+              <label htmlFor="craft">CRAFT</label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Write;
