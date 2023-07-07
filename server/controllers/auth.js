87de7054012c5db4319f191f28b1847dff077132
mohit838 <mohitul449@gmail.com>
2023-07-07 21:20:41 +0600

    update register auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,10 +7,20 @@ export const register = (req, res) => {
   db.query(q, [res.body.email, req.body.username], (err, data) => {
     if (err) return res.json(err);
     if (data.length) return res.status(400).json('User already exits!!');
-  });
 
-  const salt = bcrypt.genSaltSync(10);
-  const hashPass = bcrypt.hashSync(req.body.password, salt);
+    // Hashing Password
+    const salt = bcrypt.genSaltSync(10);
+    const hashPass = bcrypt.hashSync(req.body.password, salt);
+
+    const q = 'INSERT INTO users(`username`,`email`,`password`) VALUES (?)';
+
+    const values = [req.body.username, req.body.email, hashPass];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.json(err);
+      if (data.length) return res.status(200).json('User has been created!!');
+    });
+  });
 };
 
 export const login = (req, res) => {
